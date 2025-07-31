99e9f20dbe963f0c25156fd85eebf0222e957a01
Krish Jaiswal <krishjaiswal67890@gmail.com>
2025-08-01 00:00:22 +0530

    code

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -16,4 +16,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app); // Export analytics if you plan to use it
